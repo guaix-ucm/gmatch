fb5d9ee0d148ac836447d4a5535abb6b0dcc30a4
Sergio Pascual <sergiopr@fis.ucm.es>
2012-03-16 10:53:23 +0100

    The files I have

---
+++ matching.py
@@ -0,0 +1,61 @@
+import sys
+import numpy
+import math
+import scipy.spatial.distance as distance
+
+with open('cat1.txt') as fd:
+    cat1 = numpy.loadtxt(fd)
+
+with open('cat2.txt') as fd:
+    cat2 = numpy.loadtxt(fd)
+
+def sort_by_mag(a):
+    # http://stackoverflow.com/questions/2828059/sorting-arrays-in-numpy-by-column
+    return a[a[:,2].argsort()]
+
+cat1s = sort_by_mag(cat1)
+cat2s = sort_by_mag(cat2)
+
+def normalize1(a):
+    mi = a.min()
+    mx = a.max()
+
+    a = (a - mi) / (mx - mi)
+    return a
+
+def normaliza(a):
+    b = a[:]
+    b[:,0] = normalize1(a[:,0])
+    b[:,1] = normalize1(a[:,1])
+
+    return b
+
+def build(a1, a2, a3):
+    idx = ["12", "13", "23"]
+    r = numpy.array([a1, a2, a3])
+
+    dist = numpy.triu(distance.cdist(r, r))
+    r = dist[dist>0]
+    print r
+    i1=r.argmax()
+    print i1, idx[i1]
+    r[i1] = 0
+    i1=r.argmax()
+    print i1, idx[i1]
+    r[i1] = 0
+    i1=r.argmax()
+    print i1, idx[i1]
+    r[i1] = 0
+    sys.exit(0)
+
+a = normaliza(cat1s)
+l = cat1s.shape[0]
+for i in range(l):
+    for j in range(i + 1, l):
+        for k in range(j + 1, l):
+            build(a[i],a[j], a[k])
+            break
+
+
+
+#print normaliza(cat1s)
---
+++ simul.py
@@ -0,0 +1,41 @@
+
+import numpy
+import math
+
+coords = numpy.random.random(400)
+
+coords.shape = (200, 2)
+
+coords *= 200
+
+class Expo(object):
+    def __init__(self, m1, m2, al):
+        self.alinv = 1.0 / al
+        self.e1 = math.exp(al * m1)        
+        self.e2 = math.exp(al * m2)        
+
+    def __call__(self, x):
+        return self.alinv * numpy.log(self.e1 + self.e2 * x)
+
+e = Expo(12, 20, 1)
+
+
+
+mags = numpy.random.random(200)
+mags = e(mags)
+
+lista = numpy.zeros((200, 3))
+
+lista[:,0:2] = coords
+lista[:,2] = mags
+
+# Catalog1
+idx = (50 < lista[:,0]) & (lista[:,0] < 150) & (50 < lista[:,1]) & (lista[:,1] < 150)
+
+with open('cat1.txt', 'w') as fd:
+    numpy.savetxt(fd, lista[idx], fmt="%5.2f")
+
+# Catalog2
+idx = (53 < lista[:,0]) & (lista[:,0] < 153) & (55 < lista[:,1]) & (lista[:,1] < 155)
+with open('cat2.txt', 'w') as fd:
+    numpy.savetxt(fd, lista[idx], fmt="%5.2f")
