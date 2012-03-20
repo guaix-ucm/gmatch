e844670bea6a57a64c26dae5d3f856efbd91efeb
Sergio Pascual <sergiopr@fis.ucm.es>
2012-03-21 00:51:47 +0100

    More info in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from distutils.core import setup
 
 setup(name='gmatch',
       version='1.0',
+      description='Match pairs of objects using Groth\'s algorithm',
+      author='Sergio Pascual',
+      author_email='sergiopr@fis.ucm.es',
+      url='https://bitbucket.org/sergiopr/gmatch',
+      license='GPLv3',
       packages=['gmatch'],
       requires=['scipy'],
+      classifiers=[
+       "Programming Language :: Python",
+       'Development Status :: 3 - Alpha',
+       "Environment :: Other Environment",
+       "Intended Audience :: Science/Research",
+       "License :: OSI Approved :: GNU General Public License (GPL)",
+       "Operating System :: OS Independent",
+       "Topic :: Scientific/Engineering :: Astronomy",
+      ],
 )
